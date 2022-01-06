2d0e885d9192386893cf2733f45626668308c41d
dangomushi <>
2022-01-06 17:38:04 +0900

    bug

--- shell.c
+++ shell.c
@@ -1994,7 +1994,7 @@ void cha(int mode, struct SYSTEM3 *system3) {
 		unsigned short put[] = {'p','a','s','s'};
 		int ind = 0;
 		for (;ind<4;) {
-			putchar(moji,put[ind],system3,white);
+			print(moji,put[ind]-97,system3,white);
 			ind++;
 		}
 		print(moji,28,system3,white);
@@ -2017,7 +2017,7 @@ void cha(int mode, struct SYSTEM3 *system3) {
 					else {}
 				}
 				else {	
-					print(moji,97,system3,white);
+					print(moji,28,system3,white);
 					n++;
 				}
 		}
