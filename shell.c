bfbac8fe52af1d37c972eb1ec9ced90a056e8c16
dangomushi <>
2022-01-06 16:09:44 +0900

    bug

--- shell.c
+++ shell.c
@@ -1994,10 +1994,10 @@ void cha(int mode, struct SYSTEM3 *system3) {
 		unsigned short put[] = {'p','a','s','s'};
 		int ind = 0;
 		for (;ind<4;) {
-			print(moji,put[ind]-97,system3,white);
+			system3 = print(moji,put[ind]-97,system3,white);
 			ind++;
 		}
-		print(moji,28,system3,white);
+		system3 = print(moji,28,system3,white);
 		while (1) { 
 				puts(L"OK");
 				buf[n] = getc();
