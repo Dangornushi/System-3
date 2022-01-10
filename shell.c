267a5e087079c104a8fac4d7fc2906aa1b1ef7aa
dangomushi <>
2022-01-10 16:11:01 +0900

    number font

--- shell.c
+++ shell.c
@@ -8,7 +8,7 @@
 #define WIDTH_PER_CH	8
 #define HEIGHT_PER_CH	20
 #define MAX_COMMAND_LEN	100
-#define K_SPACE 26
+#define K_SPACE 51
 
 
 
@@ -31,7 +31,7 @@ struct CONSOLE *putchar(unsigned short moji[][12][8], unsigned short cha, struct
 	else if (cha==L' ') { c = print(moji,K_SPACE,c,black);}
 	else if (cha==K_SPACE) { c = print(moji,K_SPACE,c,black); }
 	else if (cha > 60 && cha < 96) {c = print(moji,cha-65,c,color); }//大文字
-	else if (cha < 58) { c = print(moji,cha+5,c,color); }
+	else if (cha < 58) { c = print(moji,cha+9,c,color); }
 	else if (cha < 96) { c = print(moji,cha-9,c,color); }
 	else { c = print(moji,cha-71,c,color);}//小文字
 	return c;
