61efa8239218310d1b361b708c81f4e5e72415b1
dangomushi <>
2022-01-11 16:27:35 +0900

    le bug

--- shell.c
+++ shell.c
@@ -267,7 +267,7 @@ struct CONSOLE *le(unsigned short *file_name, unsigned short moji[][12][8], stru
 			status = SFSP->OpenVolume(SFSP, &root);
 			assert(status, L"SFSP->OpenVolume");
 
-			status = root->Open(root, &file, buf+4, EFI_FILE_MODE_READ, 0);
+			status = root->Open(root, &file, file_name, EFI_FILE_MODE_READ, 0);
 			assert(status, L"root->Open");
 
 			status = file->Read(file, &buf_size, (void *)file_buf);
