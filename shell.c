c6188dd42375a3f4c9f252fcd2264d1842eac48e
dangomushi <>
2022-01-11 18:41:56 +0900

    le update

--- shell.c
+++ shell.c
@@ -276,9 +276,8 @@ struct CONSOLE *le(unsigned short *file_name, unsigned short moji[][12][8], stru
 			assert(status, L"file->Read");
 
 			for (int n=0;file_buf[n]!=L'\0';n++) {
-				if (file_buf[n]==L'\r') {enter_counter++;}
+				if (file_buf[n]==L'\r') {puts(L"OK");enter_counter++;}
 				if (enter_counter==number) {
-					puts("OK");
 					file_data[i] = file_buf[n];
 					i++;
 				}
