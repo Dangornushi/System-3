0a6b56f270fc957aa2e9b9e5522129e1a999497e
dangomushi <>
2022-01-15 14:01:43 +0900

    le update

--- shell.c
+++ shell.c
@@ -381,6 +381,7 @@ struct CONSOLE *le(unsigned short *file_name, unsigned short moji[][12][8], stru
 					c->sp-=9;
 					c = print(moji,K_SPACE,c,black);
 					c->sp-=18;
+					int[i] = 0;
 					i--;
 					ch=0;
 				}
