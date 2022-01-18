8414fcf62427fd5ff63f77fc78cd209bc2188487
dangomushi <>
2022-01-18 22:31:11 +0900

    add upAndown

--- shell.c
+++ shell.c
@@ -2166,7 +2166,24 @@ void cha(int mode, struct CONSOLE *console) {
 				else {}
 			}
 			else {	
-				if (buf[n] == L'%' || buf[n] == L'$') {
+				if (buf[n] == L'%') {
+					n=0;
+					console->sp=0;
+					for (int tmp=0;tmp<50;tmp++) { console=putchar(moji,52,console, console->back_color); }
+					console->sp=0;
+					int ind = 0;
+					for (;ind<4;) {
+						console = print(moji,put[ind]-97, console,console->char_color);
+						ind++;
+					}
+					for (;console->com_his[console->comHis_c-1-upAndown][n] != L'\0';n++) {
+						buf[n] = console->com_his[console->comHis_c-1-upAndown][n];
+						console=putchar(moji,buf[n],console,console->char_color);
+					}
+					upAndown++;
+				}
+
+				if (buf[n] == L'$') {
 					n=0;
 					console->sp=0;
 					for (int tmp=0;tmp<50;tmp++) { console=putchar(moji,52,console, console->back_color); }
@@ -2181,9 +2198,10 @@ void cha(int mode, struct CONSOLE *console) {
 						console=putchar(moji,buf[n],console,console->char_color);
 					}
 					
-					if (buf[n] == L'%')  upAndown++;
-					if ( buf[n] == L'$' ) upAndown--;
+					upAndown--;
 				}
+					
+
 				else console=putchar(moji,buf[n],console,console->char_color);
 				n++;
 			}
