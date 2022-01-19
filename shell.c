3a39b1654ac4c667989ab9dbd16dc9419cb7a5f1
dangomushi <>
2022-01-19 18:54:03 +0900

    bug

--- shell.c
+++ shell.c
@@ -2188,7 +2188,7 @@ void cha(int mode, struct CONSOLE *console) {
 					console->sp=0;
 					for (int tmp=0;tmp<50;tmp++) { console=putchar(moji,52,console, console->back_color); }
 					console->sp=0;
-					for (;tmp<4;tmp++) {
+					for (int tmp = 0;tmp<4;tmp++) {
 						console = print(moji,put[tmp]-97, console,console->char_color);
 					}
 					console->sp+=9;
