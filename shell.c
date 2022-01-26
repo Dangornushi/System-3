75406ee42dc9eee0739c4813dca8c1b3a2fbf83f
dangomushi <>
2022-01-26 22:17:48 +0900

    final commit 2

--- shell.c
+++ shell.c
@@ -574,13 +574,13 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 				int number=0;
 				unsigned short char_num[50];
 				int index = 0;
-				int add = 48;
+				int add = 30;
 
 
 				for (int tmp=0;tmp<memory[le];tmp++) {
 					if (tmp>9) {
 						index++;
-						add -= (index*10)+30;
+						add = 20+(index+10)
 					}
 					char_num[index] = tmp+add;
 				}
