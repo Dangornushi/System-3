7585a131d3367cc74d455941124645b0e452afa4
dangomushi <>
2022-01-27 18:29:51 +0900

    final commit 4(REALY FINAL)

--- shell.c
+++ shell.c
@@ -573,7 +573,7 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 				unsigned short char_num[50];
 				int index = 0;
 
-				int add = 40;
+				int add = 30;
 
 				for (int tmp=0;tmp<memory[le];tmp++) {
 					if (tmp>9) {
