cfdc067a14b77d6918ccdcc6675684083d00637f
dangomushi <>
2022-01-28 18:21:34 +0900

    final commit

--- shell.c
+++ shell.c
@@ -579,7 +579,7 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 
 				for (int tmp=0;tmp<memory[le];tmp++,tmp2++) {
 					if (tmp2>=9) {
-						char_num[0] = enter+add;
+						char_num[0] = enter+1+add;
 						if (enter>=9) { 
 							for (int tmp=0;tmp<enter;tmp++) {
 								char_num[tmp] = L'0';
