34b07d297d5e56cc71d70a8529467a17d33254b5
dangomushi <>
2022-01-30 23:53:15 +0900

    final commit

--- shell.c
+++ shell.c
@@ -609,6 +609,7 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 
 						for (int i=0;i<tc;i++) {
 							char_num[i+1] = number[10];
+							putc(number[10]);
 						}
 					}
 					else {
