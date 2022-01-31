a3e37b726dd6379c4cf54d689a508a478aa3d6af
dangomushi <>
2022-01-31 17:26:25 +0900

    final commit

--- shell.c
+++ shell.c
@@ -593,7 +593,6 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 
 				for (;tmp<memory[le];tmp++,tmp2++) {
 					if (tmp2 == add_h) {
-						tc++;
 						if (lank==10) {
 							lank = 0;
 						}
