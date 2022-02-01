57f0594337d72f93d88f3ccc2def74a66526e6ab
dangomushi <>
2022-02-01 17:34:27 +0900

    final commit

--- shell.c
+++ shell.c
@@ -617,7 +617,7 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 				//char_num[index] = number[(memory[le] % 10)-1];
 
 				//*a += L'\0';
-				tmp=4;
+				tmp=5;
 				for (;tmp>0;tmp--) {
 					c = putchar(moji, number[a[tmp]], c, c->char_color);
 				}
