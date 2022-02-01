7dacaf9439f611dcaa22e1c737fa557bb230ee5d
dangomushi <>
2022-02-01 18:00:56 +0900

    final commit

--- shell.c
+++ shell.c
@@ -618,8 +618,8 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 
 				*a += L'\0';
 
-				tmp=5;
-				for (;tmp>=0;tmp--) {
+				tmp=4;
+				for (;tmp>0;tmp--) {
 					c = putchar(moji, a[tmp], c, c->char_color);
 				}
 			}
