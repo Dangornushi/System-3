86eed22dc9ad049152e50e787d992d77ee50d228
dangomushi <>
2022-02-03 22:08:08 +0900

    touch

--- shell.c
+++ shell.c
@@ -612,8 +612,8 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 				
 				int *a = to_str(memory[le], counter);
 
-				for (;counter>=0;counter--) {
-					c = putchar(moji, &a[counter], c, c->char_color);
+				for (;tmp>=0;tmp--) {
+					c = putchar(moji, &a[tmp], c, c->char_color);
 				}
 				c->sp = 0;
 				c->ent+=13;
