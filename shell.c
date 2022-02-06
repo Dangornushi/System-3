972226c8aa31566bc3a1f16c1f266fde7f949210
dangomushi <>
2022-02-06 15:44:10 +0900

    ok

--- shell.c
+++ shell.c
@@ -632,7 +632,6 @@ void proto_run(unsigned short code[128],int j, unsigned short memory[512], struc
 			}
 
 			else if (!strcmp(L"jmp ",op)) {
-				puts(L"ok");
 				unsigned short input = get();
 				switch (input)	{
 					case SC_ESC: {
