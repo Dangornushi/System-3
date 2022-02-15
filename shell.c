9b817f893b6bc72587f1c996fb8252b754a23c46
dangomushi <>
2022-02-15 18:59:13 +0900

    add touch

--- shell.c
+++ shell.c
@@ -3740,7 +3740,7 @@ void shell(void) {
 	cons->sp = 0;
 	cons->ent = 0;
 	cons->comHis_c = 0;
-    cons->chr = moji[79][12][8];
+    cons->chr = &moji;
 	cons = startup(cons);
 	cha(mode, cons);
 }
