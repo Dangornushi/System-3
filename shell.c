d362b43e58f81c2f78f9630ce698dceccc09e803
dangomushi <>
2022-02-19 13:53:13 +0900

    OK

--- shell.c
+++ shell.c
@@ -288,7 +288,6 @@ void editer(unsigned short *file_name, struct CONSOLE *c) {
 
                 tmp=0;
                 while (1) {
-                        puts(L"OK");
                     /*選択された行を表示*/
                     for (;enter_buf[line][tmp]!=L'\n';tmp++) {
                         putchar(c->chr,enter_buf[line][tmp],c,c->char_color);
@@ -305,6 +304,7 @@ void editer(unsigned short *file_name, struct CONSOLE *c) {
                         putchar(c->chr,L'o',c,c->char_color);
                         c->sp-=9;
                     }
+                    puts(L"OK");
 
                     in_com = getc();
 
